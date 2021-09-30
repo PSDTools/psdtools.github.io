daebcd681105ef64acda74aaaf6a44b1bddc2ced
Parker Hasenkamp <91337218+ParkerH27@users.noreply.github.com>
2021-09-30 15:55:56 +0000

    Non-working, started classes/objects 9/30/20 at 10:55

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Gpa_Calculator
+Creates by: Parker Hasenkamp
+
 HTML/JS/CSS site that calculates your gpa!
 
-Repl Project:https://replit.com/@ParkerHasenkamp/GpaCalculator#index.html
+Repl Project: https://replit.com/@ParkerHasenkamp/GpaCalculator#index.html
---
+++ coursebox.js
@@ -0,0 +1,21 @@
+class Course(){
+	constructor(){
+	document.getElementById("temp").innerHTML = 
+	'
+	<textarea placeholder="Class 1" id="cl1txt"></textarea>
+	<form>
+	<select id="cl1">
+		<option value="4">A</option>
+		<option value="3">B</option>
+		<option value="2">C</option>
+		<option value="1">D</option>
+		<option value="0">F</option>
+		<option value="5">N/A</option>
+	</select>
+	<br><br>
+	</form>
+	';
+
+	}
+
+}
