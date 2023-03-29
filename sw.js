0455d58fe146e6813dfee222e580c6a7d6400a5e
Parker Hasenkamp <91337218+ParkerH27@users.noreply.github.com>
2023-03-29 19:12:24 +0000

    added precache list

--- sw.js
+++ sw.js
@@ -2,7 +2,12 @@ const CACHE_NAME = 'cool-cache';
 
 // Add whichever assets you want to pre-cache here:
 const PRECACHE_ASSETS = [
-    '/'
+	'/index.html',
+	'/help.html',
+	'/manifest.json',
+	'/psdr3-icon.png',
+	'/script.js',
+	'/style.css'
 ]
 
 // Listener for the install event - pre-caches our assets list on service worker install.
