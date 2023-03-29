60cfe001048e1fc2f644ecde53872fac4115c1a2
Parker Hasenkamp <91337218+ParkerH27@users.noreply.github.com>
2023-03-29 17:42:00 +0000

    sw changes?

--- sw.js
+++ sw.js
@@ -16,3 +16,21 @@ workbox.routing.registerRoute(
     cacheName: CACHE
   })
 );
+
+self.addEventListener('fetch', event => {
+  event.respondWith(async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
+
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+          // Cache hit, return the resource
+          return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+          return fetch(event.request)
+      };
+  });
+});
