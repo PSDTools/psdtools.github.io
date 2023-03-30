b194a2a161ac6952c89461a7393ca0196b9bcc52
Parker Hasenkamp <91337218+ParkerH27@users.noreply.github.com>
2023-03-30 13:23:53 +0000

    how about this serviceworker....

--- sw.js
+++ sw.js
@@ -1,23 +1,94 @@
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
- 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
+// I will be so happy if this works
+//https://css-tricks.com/add-a-service-worker-to-your-site/
+// Core assets
+let coreAssets = ["index.html","AppImages","manifest.json","script.js","style.css","psdr3-icon.png"];
+
+// On install, cache core assets
+self.addEventListener('install', function (event) {
+
+	// Cache core assets
+	event.waitUntil(caches.open('app').then(function (cache) {
+		for (let asset of coreAssets) {
+			cache.add(new Request(asset));
+		}
+		return cache;
+	}));
+
+});
+
+// Listen for request events
+self.addEventListener('fetch', function (event) {
+
+	// Get the request
+	let request = event.request;
+
+	// Bug fix
+	// https://stackoverflow.com/a/49719964
+	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+
+	// HTML files
+	// Network-first
+	if (request.headers.get('Accept').includes('text/html')) {
+		event.respondWith(
+			fetch(request).then(function (response) {
+
+				// Create a copy of the response and save it to the cache
+				let copy = response.clone();
+				event.waitUntil(caches.open('app').then(function (cache) {
+					return cache.put(request, copy);
+				}));
+
+				// Return the response
+				return response;
+
+			}).catch(function (error) {
+
+				// If there's no item in cache, respond with a fallback
+				return caches.match(request).then(function (response) {
+					return response || caches.match('/offline.html');
+				});
+
+			})
+		);
+	}
+
+	// CSS & JavaScript
+	// Offline-first
+	if (request.headers.get('Accept').includes('text/css') || request.headers.get('Accept').includes('text/javascript')) {
+		event.respondWith(
+			caches.match(request).then(function (response) {
+				return response || fetch(request).then(function (response) {
+
+					// Return the response
+					return response;
+
+				});
+			})
+		);
+		return;
+	}
+
+	// Images
+	// Offline-first
+	if (request.headers.get('Accept').includes('image')) {
+		event.respondWith(
+			caches.match(request).then(function (response) {
+				return response || fetch(request).then(function (response) {
+
+					// Save a copy of it in cache
+					let copy = response.clone();
+					event.waitUntil(caches.open('app').then(function (cache) {
+						return cache.put(request, copy);
+					}));
+
+					// Return the response
+					return response;
+
+				});
+			})
+		);
+	}
+
 });
 
 // const CACHE_NAME = 'cool-cache';
