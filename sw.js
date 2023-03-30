50859e25b57dddc3b5b3638215874b38db251e51
Parker Hasenkamp <91337218+ParkerH27@users.noreply.github.com>
2023-03-30 14:32:01 +0000

    web.dev sw tutorial

--- sw.js
+++ sw.js
@@ -1,55 +1,11 @@
-// This is the "Offline copy of pages" service worker
-
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+self.addEventListener('install', (event) => {
+  console.log('Inside the install handler:', event);
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-const CACHE_NAME = 'cool-cache';
-
-// Add whichever assets you want to pre-cache here:
-const PRECACHE_ASSETS = [
-    '/GPA_Calculator/script.js',
-    '/GPA_Calculator/style.css',
-		'/GPA_Calculator/AppImages/',
-		'/GPA_Calculator/psdr3-icon.png'
-]
-
-// Listener for the install event - pre-caches our assets list on service worker install.
-self.addEventListener('install', event => {
-    event.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        cache.addAll(PRECACHE_ASSETS);
-    })());
+self.addEventListener('activate', (event) => {
+  console.log('Inside the activate handler:', event);
 });
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(async () => {
-      const cache = await caches.open(CACHE_NAME);
 
-      // match the request to our cache
-      const cachedResponse = await cache.match(event.request);
-
-      // check if we got a valid response
-      if (cachedResponse !== undefined) {
-          // Cache hit, return the resource
-          return cachedResponse;
-      } else {
-        // Otherwise, go to the network
-          return fetch(event.request)
-      };
-  });
+self.addEventListener(fetch, (event) => {
+  console.log('Inside the fetch handler:', event);
 });
