59fba8ba4b914c109de7d7011e021a61c90c3aa9
Parker Hasenkamp <91337218+ParkerH27@users.noreply.github.com>
2023-03-30 13:13:56 +0000

    changed to working dynamic cache???

--- sw.js
+++ sw.js
@@ -1,39 +1,61 @@
-const CACHE_NAME = 'cool-cache';
 
-// Add whichever assets you want to pre-cache here:
-const PRECACHE_ASSETS = [
-	'/index.html',
-	'/help.html',
-	'/manifest.json',
-	'/psdr3-icon.png',
-	'/script.js',
-	'/style.css'
-]
+event.respondWith((async () => {
+  const cachedResponse = await caches.match(event.request);
+  if (cachedResponse) {
+    return cachedResponse;
+  }
 
-// Listener for the install event - pre-caches our assets list on service worker install.
-self.addEventListener('install', event => {
-    event.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        cache.addAll(PRECACHE_ASSETS);
-    })());
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(async () => {
-      const cache = await caches.open(CACHE_NAME);
+  const response = await fetch(event.request);
 
-      // match the request to our cache
-      const cachedResponse = await cache.match(event.request);
+  if (!response || response.status !== 200 || response.type !== 'basic') {
+    return response;
+  }
 
-      // check if we got a valid response
-      if (cachedResponse !== undefined) {
-          // Cache hit, return the resource
-          return cachedResponse;
-      } else {
-        // Otherwise, go to the network
-          return fetch(event.request)
-      };
-  });
-});
+  if (ENABLE_DYNAMIC_CACHING) {
+    const responseToCache = response.clone();
+    const cache = await caches.open(DYNAMIC_CACHE)
+    await cache.put(event.request, response.clone());
+  }
+
+  return response;
+})());
+
+// const CACHE_NAME = 'cool-cache';
+
+// // Add whichever assets you want to pre-cache here:
+// const PRECACHE_ASSETS = [
+// 	'/index.html',
+// 	'/help.html',
+// 	'/manifest.json',
+// 	'/psdr3-icon.png',
+// 	'/script.js',
+// 	'/style.css'
+// ]
+
+// // Listener for the install event - pre-caches our assets list on service worker install.
+// self.addEventListener('install', event => {
+//     event.waitUntil((async () => {
+//         const cache = await caches.open(CACHE_NAME);
+//         cache.addAll(PRECACHE_ASSETS);
+//     })());
+// });
+// self.addEventListener('activate', event => {
+//   event.waitUntil(clients.claim());
+// });
+// self.addEventListener('fetch', event => {
+//   event.respondWith(async () => {
+//       const cache = await caches.open(CACHE_NAME);
+
+//       // match the request to our cache
+//       const cachedResponse = await cache.match(event.request);
+
+//       // check if we got a valid response
+//       if (cachedResponse !== undefined) {
+//           // Cache hit, return the resource
+//           return cachedResponse;
+//       } else {
+//         // Otherwise, go to the network
+//           return fetch(event.request)
+//       };
+//   });
+// });
