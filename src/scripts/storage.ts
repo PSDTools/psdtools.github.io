b4777ba78bb9f8c2a2775cca0e4783ddc7ca15de
Eli <88557639+lishaduck@users.noreply.github.com>
2023-07-15 20:16:30 -0500

    fix: it!

--- src/scripts/storage.ts
+++ src/scripts/storage.ts
@@ -1,19 +1,19 @@
 /** Clears class storage data. */
 function clearData(): void {
-  localStorage.removeItem("arraycookie");
+  localStorage.removeItem("arraystorage");
   location.reload();
 }
 
 /** Clears all website storage data. */
 function clearAll(): void {
-  localStorage.removeItem("gradecookie");
+  localStorage.removeItem("gradestorage");
   localStorage.removeItem("color");
   localStorage.removeItem("shade");
   window.clearData();
 }
 
-function setData(arraycookie: string): void {
-  localStorage.setItem("arraycookie", arraycookie);
+function setData(arraystorage: string): void {
+  localStorage.setItem("arraystorage", arraystorage);
 }
 
 export { clearData, clearAll, setData };
