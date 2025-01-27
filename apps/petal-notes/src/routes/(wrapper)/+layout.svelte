6f7ff41de56db77225fe0dff9001e43bd8488959
Eli <88557639+lishaduck@users.noreply.github.com>
2025-01-26 19:42:23 -0600

    feat: light mode
    Bro... it was too blindingly black. This is nicer.

--- apps/petal-notes/src/routes/(wrapper)/+layout.svelte
+++ apps/petal-notes/src/routes/(wrapper)/+layout.svelte
@@ -4,20 +4,22 @@
   let { children, data } = $props();
 </script>
 
-<div class="wrapper flex h-screen flex-col overflow-hidden bg-slate-950">
-  <header class="flex w-full items-center bg-slate-950 pl-4 pt-4 text-white">
+<div
+  class="wrapper flex h-screen flex-col overflow-hidden bg-slate-50 text-slate-950 dark:bg-slate-950 dark:text-slate-50"
+>
+  <header class="flex w-full items-center pl-4 pt-4">
     <a class="flex items-center space-x-2" href="/">
       <img class="h-8 w-8" alt="PetalNotes Logo" src="/PetalNotes-logo.svg" />
       <h1>Petal Notes</h1>
     </a>
   </header>
   <div class="flex flex-1 overflow-hidden">
-    <aside class="w-64 bg-slate-950 p-4 text-white">
+    <aside class="w-64">
       <!-- Sidebar tree -->
       <!-- eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -->
       <TreeView isRoot item={data.root} />
     </aside>
-    <main class="bg-back flex-1 overflow-auto rounded-tl-lg p-2 text-white">
+    <main class="flex-1 overflow-auto rounded-tl-lg p-2">
       {@render children()}
     </main>
   </div>
--- apps/petal-notes/src/routes/(wrapper)/notes/[...path]/+page.svelte
+++ apps/petal-notes/src/routes/(wrapper)/notes/[...path]/+page.svelte
@@ -1,35 +1,33 @@
 <script lang="ts">
   import HyperMDC from "$lib/hypermd.svelte";
 
-  let mode: "hypermd" | "normal" = $state("hypermd");
+  type EditorMode = "hypermd" | "normal";
+
+  let editorMode: EditorMode = $state("hypermd");
 
   let { data } = $props();
 </script>
 
 <div class="p-1">
-  <button
-    class="rounded-sm border px-2 py-0.5"
-    onclick={() => {
-      mode = "normal";
-    }}
-    type="button"
-  >
-    Switch To Normal
-  </button>
-  <button
-    class="rounded-md border px-2 py-0.5"
-    onclick={() => {
-      mode = "hypermd";
-    }}
-    type="button"
-  >
-    Switch To HyperMD
-  </button>
+  {#snippet button(mode: EditorMode, modeName: string)}
+    <button
+      class="rounded-sm border border-slate-950 px-2 py-0.5 dark:border-slate-50"
+      onclick={() => {
+        editorMode = mode;
+      }}
+      type="button"
+    >
+      Switch To {modeName}
+    </button>
+  {/snippet}
+
+  {@render button("normal", "Normal")}
+  {@render button("hypermd", "HyperMD")}
 </div>
 
 <div id="hypermd-container">
   <!-- eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -->
-  <HyperMDC value={data.note} bind:mode />
+  <HyperMDC value={data.note} bind:mode={editorMode} />
 </div>
 
 <style>
