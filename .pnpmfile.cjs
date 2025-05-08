303d4c09f54c0af1b4569abe2ef71e2b81b3361f
Eli <88557639+lishaduck@users.noreply.github.com>
2025-05-07 21:31:04 -0500

    chore: debug renovate more

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,2 +1,3 @@
 // @ts-check
-module.exports = require("./node_modules/.pnpm-config/@pnpmfiles/nolyfill/.pnpmfile.cjs");
+// module.exports = require("./node_modules/.pnpm-config/@pnpmfiles/nolyfill/.pnpmfile.cjs");
+module.exports = {};
